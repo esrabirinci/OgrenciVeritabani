8733ce055e92be9b5763346bb28e071ce15d037a
Esra Birinci <esrabirinci2010@gmail.com>
2022-12-24 15:51:43 +0300

    Proje

---
+++ ogrenci_veritabani_uygulamasi/Ogrenci_Veritabani.java
@@ -0,0 +1,13 @@
+package git2.ogrenci_veritabani_uygulamasi;
+
+public class Ogrenci_Veritabani {
+    public static void main(String[] args) {
+        //Constructor ekleme
+        Ogrenci ogr = new Ogrenci();
+        ogr.ogrenciNoBelirle();
+        ogr.dersKaydi();
+        ogr.odemeMitari();
+        System.out.println(Ogrenci.ANSI_YELLOW + "--------------------------");
+        System.out.println(Ogrenci.ANSI_GREEN + Ogrenci.ANSI_BOLD + "Kaydınız başarılı bir şekilde tamamlanmıştır.\nDerslerinizi sorumlu danışmanınıza onaylatınız.");
+    }
+}
